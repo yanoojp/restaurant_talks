60ed70ec8b548272f976b2274c62582769114d00
yanoojp <120234755+yanoojp@users.noreply.github.com>
2023-08-06 14:58:52 +0900

    define variables

---
+++ lib/utils/variables.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+// 画面タイトル
+const String appTitle = 'Restaurant Talks';
+
+// 色
+const Color darkBlue = Color(0xFF28527a);
+const Color lightBlue = Color(0xFF8ac4d0);
+const Color darkYellow = Color(0xFFf4d160);
+const Color lightYellow = Color(0xFFfbeeac);
+const Color whiteColor = Color(0xFFffffff);
+const Color blackColor = Color(0xFF000000);
+
+// 数字
+const double appTitleFontSize = 30;
