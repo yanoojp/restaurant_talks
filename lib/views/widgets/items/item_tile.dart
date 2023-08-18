1df9c8aae502e1fc323e60f211e754bf7ca76187
yanoojp <120234755+yanoojp@users.noreply.github.com>
2023-08-18 09:37:29 +0900

    adjust to use simulation data

--- lib/views/widgets/items/item_tile.dart
+++ lib/views/widgets/items/item_tile.dart
@@ -1,33 +1,33 @@
 import 'package:flutter/material.dart';
 import '../../../constants/variables.dart';
+import '../../../models/Iitems/item_model.dart';
 
 class ItemTile extends StatelessWidget {
-  final int index;
+  final Item item;
 
-  const ItemTile({super.key, required this.index});
+  const ItemTile({super.key, required this.item});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.all(10),
       decoration: BoxDecoration(
         color: darkBlue,
         borderRadius: BorderRadius.circular(8.0),
       ),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              "Item Name $index",
-              style: const TextStyle(color: whiteColor),
-            ),
-            const Text(
-              "Item Stock",
-              style: TextStyle(color: whiteColor),
-            ),
-          ],
-        ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            item.name,
+            style: const TextStyle(color: whiteColor), textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 10,),
+          Text(
+            '${item.stockCount}',
+            style: const TextStyle(color: whiteColor), textAlign: TextAlign.center,
+          ),
+        ],
       ),
     );
   }
