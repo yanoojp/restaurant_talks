c2df1a04781d4bb033644d6ede5c8f6a45b5979e
yanoojp <120234755+yanoojp@users.noreply.github.com>
2023-09-03 18:12:38 +0900

    revert

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,12 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:restaurant_talks/constants/variables.dart';
-import 'package:restaurant_talks/firebase_options.dart';
 import 'package:restaurant_talks/routes/app_routes.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  runApp(const MyApp());
+void main() {
+  runApp(const ProviderScope(
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends ConsumerWidget {
