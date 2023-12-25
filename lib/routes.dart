1871a866b0350fdd84550a02aa4b4531925dd714
yanoojp <120234755+yanoojp@users.noreply.github.com>
2023-12-26 07:50:40 +0900

    use go router

---
+++ lib/routes.dart
@@ -0,0 +1,16 @@
+import 'package:go_router/go_router.dart';
+import 'package:restauratn_talks/views/screens/login_screen.dart';
+import 'package:restauratn_talks/views/screens/sign_up_screen.dart';
+
+final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        path: '/sign_up',
+        builder: (context, state) => const SignUpScreen(),
+      ),
+    ],
+  );
