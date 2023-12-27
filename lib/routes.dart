dafc44743351d45f5f80adc50aebcc8ee308e0e4
yanoojp <120234755+yanoojp@users.noreply.github.com>
2023-12-27 10:37:33 +0900

    add routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,16 +1,38 @@
 import 'package:go_router/go_router.dart';
-import 'package:restauratn_talks/views/screens/login_screen.dart';
-import 'package:restauratn_talks/views/screens/sign_up_screen.dart';
+import 'package:restauratn_talks/views/screens/guest_numbers/guest_number_screen.dart';
+import 'package:restauratn_talks/views/screens/items/item_form_screen.dart';
+import 'package:restauratn_talks/views/screens/items/item_index_screen.dart';
+import 'package:restauratn_talks/views/screens/users/login_screen.dart';
+import 'package:restauratn_talks/views/screens/users/sign_up_screen.dart';
+import 'package:restauratn_talks/views/screens/users/user_info_screen.dart';
 
 final router = GoRouter(
-    routes: [
-      GoRoute(
-        path: '/',
-        builder: (context, state) => const LoginScreen(),
-      ),
-      GoRoute(
-        path: '/sign_up',
-        builder: (context, state) => const SignUpScreen(),
-      ),
-    ],
-  );
+  routes: [
+    GoRoute(
+      path: '/login',
+      builder: (context, state) => const LoginScreen(),
+    ),
+    GoRoute(
+      path: '/sign_up',
+      builder: (context, state) => const SignUpScreen(),
+    ),
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const ItemIndexScreen(),
+      routes: [
+        GoRoute(
+          path: 'form',
+          builder: (context, state) => const ItemFormScreen(),
+        ),
+      ],
+    ),
+    GoRoute(
+      path: '/user_info',
+      builder: (context, state) => const UserInfoScreen(),
+    ),
+    GoRoute(
+      path: '/guest_number',
+      builder: (context, state) => const GuestNumberScreen(),
+    ),
+  ],
+);
